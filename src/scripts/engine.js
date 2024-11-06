6e0c03e95ef594605df684063f9ca51a8227787a
mtheusmr <ma.theus_123@hotmail.com>
2024-11-05 23:45:24 -0300

    Finalizando etapa do projeto de JS, da DIO de HTML, CSS e Javascript

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,9 +6,35 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null
+        timerId: null,
+        countDownTimerId: setInterval(countDown, 1000),
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result:0,
+        curretTime: 60,
     },
-};
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+}
+
+function playSound(){
+    let audio = new Audio("/src/audios/src_audios_hit.m4a")
+    audio.volume = 0.2,
+    audio.play()
+}
+
+function countDown() {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime
+
+    if (state.values.curretTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.TimerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
 
 function randomSquare(){
     state.view.squares.forEach((square)=> {
@@ -18,18 +44,29 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy(){
-    state.values.timerId = setInterval(randomSquare, 1000)
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 }
 
 function addListenerHitBox(){
-    state.view.squares.forEach((square)=> {});
+    state.view.squares.forEach((square)=> {
+        square.addEventListener("mousedown", ()=> {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound();
+            };
+        });
+    });
 }
 
 function initialize() {
     moveEnemy();
+    addListenerHitBox();
 }
 
 initialize ();
